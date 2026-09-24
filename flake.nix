3d7cdc3697fc476f6099eb14e084c48a1bf9a415
mrChest228 <gengenm32111111@gmail.com>
2026-09-24 22:29:19 +0300

    Flake fix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,25 @@
 {
     # A simple building tree of files in set for easy imports
-    outputs = (path: let
-        ls = builtins.readDir path;
-        treeWithNulls = builtins.mapAttrs (name: type:
-            if type == "directory" then
-                mkFileTree "${path}/${name}"
-            else if type == "regular" && builtins.match ".*//.nix" name != null then
-                "${path}/${name}"
-            else
-                null
-        ) ls;
-        tree = builtins.filterAttrs (k: v: v != null) treeWithNulls;
+    outputs = let
+        mkFileTree = (path: let
+            ls = builtins.readDir path;
+            treeWithNulls = builtins.mapAttrs (name: type:
+                if type == "directory" then
+                    mkFileTree "${path}/${name}"
+                else if type == "regular" && builtins.match ".*//.nix" name != null then
+                    "${path}/${name}"
+                else
+                    null
+            ) ls;
+            tree = builtins.filterAttrs (k: v: v != null) treeWithNulls;
 
-        topLevel = { imports = (builtins.filter (x: builtins.isPath x) (builtins.attrValues tree))};
-        all = { imports = (topLevel.imports ++ (lib.flatten (builitins.map (folder: folder.all.imports) (builtins.filter (x: builtins.isAttrs x) (builtins.attrValues tree))))) };
-    in
-        tree // { inherit all topLevel; };
-    }) ./.;
+            topLevel = { imports = (builtins.filter (x: builtins.isPath x) (builtins.attrValues tree))};
+            all = { imports = (topLevel.imports ++ (lib.flatten (builitins.map (folder: folder.all.imports) (builtins.filter (x: builtins.isAttrs x) (builtins.attrValues tree))))) };
+        in
+            tree // { inherit all topLevel; };
+        });
+    in {
+        sys = mkFileTree ./sys;
+        hm = mkFileTree ./hm;
+    };
 }
